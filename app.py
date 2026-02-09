b4135970d3e6e532588237e3151aa45caf35d417
Shane McDonald <me@shanemcd.com>
2026-02-09 18:15:35 -0500

    Add farewell function

--- app.py
+++ app.py
@@ -5,5 +5,10 @@ def greet(name: str) -> str:
     return f"Hello, {name}!"
 
 
+def farewell(name: str) -> str:
+    return f"Goodbye, {name}!"
+
+
 if __name__ == "__main__":
     print(greet("world"))
+    print(farewell("world"))
